d8a2f16beb7e03d01944ec7103fdaf68d0486221
bcondict <jesus.junc10@gmail.com>
2025-04-25 17:03:02 -0500

    update (astro config file): removed node adapter, make it csr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import node from "@astrojs/node";
+// import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: node({
-    mode: "standalone",
-  }),
+  // adapter: node({
+  //   mode: "standalone",
+  // }),
   site: "https://bcondict.github.io",
   base: "bloque-techTest-monolithic",
 });
-
