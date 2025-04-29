7956edfc4ac688883df6bde906529a33834b5fd7
bcondict <jesus.junc10@gmail.com>
2025-04-28 22:49:14 -0500

    update (astro config file - index): fetch is now made on client, using vite pwa to handle the service worker

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import VitePWA from "@vite-pwa/astro";
 
 export default defineConfig({
   site: "https://bcondict.github.io",
   base: "bloque-techTest-monolithic",
+  integrations: [
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Bloque tech test",
+        short_name: "App",
+      },
+    }),
+  ],
 });
--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,30 +1,53 @@
 const CACHE_NAME = "my_cache";
 
-const urlToCache = ["/", "index.astro", "bloque-techTest-monolithic"];
+const urlToCache = ["/bloque-techTest-monolithic"];
+
+const filesUpdate = (cache) => {
+  const stack = [];
+  urlToCache.forEach((file) =>
+    stack.push(
+      cache
+        .add(file)
+        .catch((_) => console.error(`can't load ${file} to cache`)),
+    ),
+  );
+  return Promise.all(stack);
+};
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("Cache open");
-      return cache.addAll(urlToCache);
-    }),
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(filesUpdate));
+  // event.waitUntil(
+  //   caches
+  //     .open(CACHE_NAME)
+  //     .then((cache) => {
+  //       console.log("Cache open");
+  //       return cache.addAll(urlToCache);
+  //     })
+  //     .catch((error) => {
+  //       console.error("error: ", error);
+  //     }),
+  // );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
-    }),
+    caches
+      .match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+      .catch((error) => {
+        console.error("error: ", error);
+      }),
   );
 });
 
 self.addEventListener("activate", (event) => {
   const cacheWhitelist = [CACHE_NAME];
-  event.waitUtil(
+  event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
